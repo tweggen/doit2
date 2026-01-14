59533da2b4f14b09eae87521026cd588f6d9d1c1
tweggen <timo.weggen@gmail.com>
2026-01-14 18:08:22 +0100

    - more work on appearance-

---
+++ DoitBlazor/Configuration/AppConfig.cs
@@ -0,0 +1,14 @@
+namespace DoitBlazor.Configuration;
+
+public static class AppConfig
+{
+    /// <summary>
+    /// The application brand name used throughout the UI
+    /// </summary>
+    public const string BrandName = "Geoffrey";
+    
+    /// <summary>
+    /// The application tagline
+    /// </summary>
+    public const string Tagline = "Your personal task management application";
+}
